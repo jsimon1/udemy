1113e9ef7b8c3e3c96039ec7919224d38f7eb981
Jeremy Simon <jeremysimon2@gmail.com>
2017-06-03 21:27:27 -0400

    NodeJSCourse Lecture 30 app.js creation

--- NodeJSCourse/weather-app/app.js
+++ NodeJSCourse/weather-app/app.js
@@ -1,14 +1,25 @@
 const request = require('request');
 
-// Sample request - can define options in first parameter, second is a callback.
-// JSON.stringify takes the object as its first paramter, a filter as its second paramter, and the number of spaces
-// to pretty print the JSON (instead of it just being a garbled paragraph of objects)
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=6%20Myrtle%20Street%20Montvale',
   json: true
 }, (err, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+  console.log(`Long: ${body.results[0].geometry.location.lng}`);
 });
+
+// Sample request - can define options in first parameter, second is a callback.
+// JSON.stringify takes the object as its first paramter, a filter as its second paramter, and the number of spaces
+// to pretty print the JSON (instead of it just being a garbled paragraph of objects)
+//
+// request({
+//   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=6%20Myrtle%20Street%20Montvale',
+//   json: true
+// }, (err, response, body) => {
+//   console.log(JSON.stringify(body, undefined, 2));
+// });
+//
 // Response contains status code, body, headers (api -> node), request object (node -> api)
 // console.log(response);
 
