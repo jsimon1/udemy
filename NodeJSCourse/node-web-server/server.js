43354f5c7ba5cace758040a3478a937f12643099
Jeremy Simon <jeremysimon2@gmail.com>
2017-06-07 18:35:25 -0400

    Lecture 45 - Handlebars added - Express Middleware continued

--- NodeJSCourse/node-web-server/server.js
+++ NodeJSCourse/node-web-server/server.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 app.set('view engine', 'hbs');
 
 // Express middle-ware (express works one way, we want some things about it to change)
+// These fire whenever a request is made
 app.use(express.static(__dirname + '/src'));
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log +'\n', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
 
 app.get('/', (req, res) => {
   // Send HTML
@@ -14,15 +38,13 @@ app.get('/', (req, res) => {
   // Send JSON Data
   res.render('home.hbs',{
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
-    currYear: new Date().getFullYear()
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
