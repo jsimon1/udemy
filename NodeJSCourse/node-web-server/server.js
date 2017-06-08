c2ca2fdeb0beb722803b91d2d048fe21ada6629d
Jeremy Simon <jeremysimon2@gmail.com>
2017-06-07 21:19:49 -0400

    Setup start script and Heroku port

--- NodeJSCourse/node-web-server/server.js
+++ NodeJSCourse/node-web-server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+// port is configured for heroku port and also on local machine on 3000
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -55,5 +57,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log(`Server listening on port ${port}`);
 });
